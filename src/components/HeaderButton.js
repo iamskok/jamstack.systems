656e2f0fbff0a28406bc3c5d21222072159a0bf0
Vladimir Skok <skok@vova.io>
2019-09-02 23:18:16 -0700

    feat: add basic styles for HeaderButton

--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -4,11 +4,20 @@ import { useColorMode } from 'theme-ui'
 export default props => {
   const [colorMode, setColorMode] = useColorMode()
   return (
-    <header>
+    <header
+      style={{
+        top: '0',
+        right: '0',
+        position: 'absolute',
+        padding: '0',
+        margin: '0'
+      }}
+    >
       <button
         onClick={e => {
           setColorMode(colorMode === 'light' ? 'dark' : 'light')
-        }}>
+        }}
+      >
         Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
       </button>
     </header>
