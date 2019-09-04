a5f29800012aa3876874cb4595d67c1484c0d626
Vladimir Skok <skok@vova.io>
2019-09-03 22:26:44 -0700

    feat: update theme-ui config

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,16 +1,16 @@
 import baseTheme from 'gatsby-theme-mdx-deck/src/gatsby-plugin-theme-ui'
-import { lobster } from '@mdx-deck/themes'
+// import { yellow } from '@mdx-deck/themes'
 
 export default {
   ...baseTheme,
-  ...lobster,
   initialColorMode: 'light',
   // optionally enable custom properties
   // to help avoid a flash of colors on page load
   useCustomProperties: true,
   colors: {
+    primary: '#bf4080',
     text: '#222',
-    background: '#fff',
+    background: '#ffe000',
     modes: {
       dark: {
         text: ' #fff',
