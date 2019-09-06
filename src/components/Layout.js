7966ab623ba6917992f10e1043261d5a497b9e74
Vladimir Skok <skok@vova.io>
2019-09-05 19:26:48 -0700

    refactor: rename HeaderButton => ThemeToggle component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import HeaderButton from './HeaderButton'
+import ThemeToggle from './ThemeToggle'
 
 export default ({ children }) => (
   <>
-    <HeaderButton />
+    <ThemeToggle />
 
     { children }
   </>
