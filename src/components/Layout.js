fe24368d0dc93316ffb9e773f2a972a0adbb4556
Vladimir <vladimir.skok@bvaccel.com>
2019-09-21 11:32:04 -0700

    feat: add max-width to Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,11 @@
-import React from 'react'
+/** @jsx jsx */
 import ThemeToggle from './ThemeToggle'
+import { jsx } from 'theme-ui'
 
 export default ({ children }) => (
-  <>
+  <div sx={{ maxWidth: '80vw' }}>
     <ThemeToggle />
 
     { children }
-  </>
+  </div>
 )
