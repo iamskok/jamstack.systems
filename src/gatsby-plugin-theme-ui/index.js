6747ea8d223a012eab5beb1b1c4f69857fc40f73
Vladimir <vladimir.skok@bvaccel.com>
2019-09-25 21:36:20 -0700

    fix: theme-ui link config

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -141,7 +141,6 @@ export default {
       fontSize: 1
     },
     a: {
-      fontSize: 2,
       fontWeight: `link`,
       color: `primary`,
       transition: 'color 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94)',
