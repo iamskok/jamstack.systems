ee114c23d7fa0f122deed8c60f6ad8574cc7ef5b
Vladimir <vladimir.skok@bvaccel.com>
2019-09-27 22:43:09 -0700

    feat: update Stack slide image

---
+++ src/components/images/Stacks.js
@@ -0,0 +1,29 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import { graphql, useStaticQuery } from 'gatsby'
+import Image from 'gatsby-image'
+
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      image: file(name: { eq: "lamp-mean-jam" }) {
+        cloudinary: childCloudinaryAsset {
+          fluid {
+            ...CloudinaryAssetFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Image
+      fluid={data.image.cloudinary.fluid}
+      alt='JAMstack Ecosystem'
+      sx={{
+        margin: 'auto',
+        maxWidth: '750px'
+      }}
+    />
+  )
+}
