8856a1a9e9929d2b8dbb88f3f758d05d7df5fe8b
Vladimir <vladimir.skok@bvaccel.com>
2019-09-27 22:42:54 -0700

    feat: update Ecosystem slide image

---
+++ src/components/images/Ecosystem.js
@@ -0,0 +1,30 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import Image from 'gatsby-image'
+
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      image: file(name: { eq: "jamstack-ecosystem" }) {
+        cloudinary: childCloudinaryAsset {
+          fluid {
+            ...CloudinaryAssetFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Image
+      fluid={data.image.cloudinary.fluid}
+      alt='JAMstack Ecosystem'
+      sx={{
+        margin: 'auto',
+        maxWidth: '750px'
+      }}
+    />
+  )
+}
