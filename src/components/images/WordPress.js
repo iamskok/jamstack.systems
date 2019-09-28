493859b904cb9bc20b1e376e835348552089149f
Vladimir <vladimir.skok@bvaccel.com>
2019-09-27 21:44:27 -0700

    feat: update Limited Flexability slide

---
+++ src/components/images/WordPress.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import Image from 'gatsby-image';
+
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      image: file(name: { eq: "wordpress-logo" }) {
+        cloudinary: childCloudinaryAsset {
+          fixed(width: 200) {
+            ...CloudinaryAssetFixed
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Image
+      fixed={data.image.cloudinary.fixed}
+      alt="WordPress Logo"
+    />
+  )
+}
