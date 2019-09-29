1f3535549cd3299e6b339f4f863a54dec6912387
Vladimir <vladimir.skok@bvaccel.com>
2019-09-28 22:11:30 -0700

    feat: update Header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,28 +5,34 @@ import Logo from './Logo'
 import ThemeToggle from './ThemeToggle'
 
 export default ({ opened, close }) => (
-  <div>
-    <TOC opened={ opened } />
-    <header sx={{
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%'
-    }}>
-      <nav sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '100%',
-        px: 2,
-        py: 2,
-      }}>
+  <header>
+    <div
+      sx={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%'
+      }}
+    >
+      <nav
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '100%',
+          px: 2,
+          py: 2,
+        }}
+      >
         <Logo
           opened={ opened }
           close={ close }
         />
+
         <ThemeToggle />
       </nav>
-    </header>
-  </div>
+    </div>
+
+    <TOC opened={ opened } />
+  </header>
 )
