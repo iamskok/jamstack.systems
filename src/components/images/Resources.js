bb1563c86cb629103282a5cabed14ac204885143
Vladimir <vladimir.skok@bvaccel.com>
2019-10-02 00:14:26 -0700

    feat: update Resources slide

--- src/components/images/Resources.js
+++ src/components/images/Resources.js
@@ -1,4 +1,5 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 import { graphql, useStaticQuery } from 'gatsby'
 import Image from 'gatsby-image'
 
@@ -18,7 +19,10 @@ export default () => {
   return (
     <Image
       fluid={ data.file.childCloudinaryAsset.fluid }
-      alt='JamStack Resources'
+      alt='JAMstack Resources'
+      sx={{
+        maxWidth: '500px'
+      }}
     />
   )
 }
