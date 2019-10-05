245db483ce107bee55f4e46c9be564764b431184
Vladimir <vladimir.skok@bvaccel.com>
2019-10-05 13:28:14 -0700

    refactor: gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,10 @@
+const { siteMetadata } = require('./site-config')
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
 module.exports = {
-  siteMetadata: {
-    title: 'JAMstack.systems',
-    description: 'Modern Web Development on the JAMstack.',
-    siteUrl: 'https://jamstack.systems',
-    image: 'https://res.cloudinary.com/iamskok/image/upload/v1570286627/dzwoxwzc5hafl8fsr51d.jpg',
-    imageAlt: 'Multiple glass jam jars on brown wooden table',
-    name: 'Vladimir Skok',
-    twitter: "@iamskok",
-    language: 'en'
-  },
+  siteMetadata,
   plugins: [
     {
       resolve: 'gatsby-theme-mdx-deck',
---
+++ site-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: 'JAMstack.systems',
+    description: 'Gatsby MDX Deck about Modern Web Development on the JAMstack.',
+    siteUrl: 'https://jamstack.systems',
+    image: 'https://res.cloudinary.com/iamskok/image/upload/v1570286627/dzwoxwzc5hafl8fsr51d.jpg',
+    imageAlt: 'Multiple glass jam jars on brown wooden table',
+    name: 'Vladimir Skok',
+    twitter: "@iamskok",
+    language: 'en'
+  }
+}
